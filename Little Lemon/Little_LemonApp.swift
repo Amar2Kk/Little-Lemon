a26089c86f0a2ed195c36df835e6c5e1d6c0652a
Ammar Abbas <95864103+Amar2Kk@users.noreply.github.com>
2024-05-13 23:06:24 +0300

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Ammar Abbas on 13/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
